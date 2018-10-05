f8f017ddb63f7218c651b4e72e6dc024ea657442
Sujit Singh <Sujit.Singh@jameshardie.com>
2018-10-04 19:35:13 -0700

    homework

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "va72s2dr8kA6MuhTEfwa34k9K"
+consumer_secret = "nnWf7Mv7zxzJEBaISdnxETOZ6Be8xFAEe0S9nx7ZdrRugZzAF5"
+access_token = "1031667736600952833-lX4nNRBYQBqe8bDobkYpsnbcWUWu8Z"
+access_token_secret = "j3agXmTPqztQQmOhFLpZSD1SJTClNJY9L9kfh57hjb9WX"
